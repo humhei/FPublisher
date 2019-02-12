3807fc731cdf3e134bb524bb57a8278780978e56
humhei <humhei@outlook.com>
2019-02-12 15:38:22 +0800

    CI

---
+++ src/FPublisher.Expecto/Library.fs
@@ -0,0 +1,23 @@
+﻿namespace FPublisher
+open Expecto
+open System
+open System.IO
+open System.Reflection
+
+[<RequireQualifiedAccess>]
+module ExpectoConfig =
+    let appendSummaryHandler argv (testConfig: Impl.ExpectoConfig) =
+        argv
+        |> Array.tryFind (fun arg ->
+            String.Compare(arg,"--summary",true) = 0
+        )
+        |> function
+            | Some _ ->
+                let assemblyName =
+                    Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly().CodeBase)
+
+                let xmlFile = assemblyName + ".TestResults.xml"
+
+                let writeResults = TestResults.writeNUnitSummary (xmlFile, assemblyName)
+                testConfig.appendSummaryHandler writeResults
+            | None -> testConfig
