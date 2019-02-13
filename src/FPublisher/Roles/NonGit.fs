c3600d8d9d4ddfbdc35a8fabc53ba90608648c70
humhei <humhei@outlook.com>
2019-02-13 12:43:15 +0800

    Ci

--- src/FPublisher/Roles/NonGit.fs
+++ src/FPublisher/Roles/NonGit.fs
@@ -4,14 +4,14 @@ open FPublisher.Nuget
 open Fake.IO.FileSystemOperators
 open FPublisher.Git
 open Primitives
-
+open Fake.IO.Globbing.Operators
 
 [<RequireQualifiedAccess>]
 module NonGit =
 
     [<RequireQualifiedAccess>]
     type Msg =
-        | Build 
+        | Build
         | Test
 
     type TargetState =
@@ -21,36 +21,41 @@ module NonGit =
     [<RequireQualifiedAccess>]
     module TargetState =
         let init =
-            { Build = State.Init 
+            { Build = State.Init
               Test = State.Init }
 
     type Role =
-        { Solution: Solution 
+        { Solution: Solution
           Workspace: Workspace
           TargetState: TargetState }
-    with 
+    with
         interface IRole<TargetState>
-            
-    let create loggerLevel (workspace: Workspace) = 
+
+    let create loggerLevel (workspace: Workspace) =
         logger <- Logger.create(loggerLevel)
 
-        let slnPath = 
+        let slnPath =
+
             let workingDir = workspace.WorkingDir
-            workingDir </> ( workspace.DefaultSlnName + ".FPublisher" + ".sln")
+            !! (workingDir + "./*.sln")
+            |> Seq.tryFind (fun sln -> sln.EndsWith ".FPublisher.sln")
+            |> function
+                | Some slnPath -> slnPath
+                | None -> workingDir </> ( workspace.DefaultSlnName + ".FPublisher" + ".sln")
 
         Workspace.createSlnWith slnPath false workspace
-                
-        { Solution = Solution.read slnPath   
+
+        { Solution = Solution.read slnPath
           Workspace = workspace
           TargetState = TargetState.init }
-        
+
 
     let run =
-        Role.update (function 
+        Role.update (function
             | Msg.Build ->
                 { PreviousMsgs = []
-                  Action = MapState (fun role -> Solution.build role.Solution) } 
-            | Msg.Test -> 
+                  Action = MapState (fun role -> Solution.build role.Solution) }
+            | Msg.Test ->
                 { PreviousMsgs = [Msg.Build]
-                  Action = MapState (fun role -> Solution.test role.Solution) } 
+                  Action = MapState (fun role -> Solution.test role.Solution) }
         )
